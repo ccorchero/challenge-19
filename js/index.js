df04fd6a7ad36ac75d43caf195a31b60a8915896
ccorchero <141979140+ccorchero@users.noreply.github.com>
2024-01-18 16:56:13 -0800

    interactive list items

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $("li").hover(function(){
+      $(this).css("background-color", "#f3f3f3");
+      }, function(){
+      $(this).css("background-color", "#88f057");
+    });
+  });

26268baf8b39f8bcc92b598d7b438fa4bfb02bea
ccorchero <141979140+ccorchero@users.noreply.github.com>
2024-01-22 17:55:15 -0800

    We added hover event on menu tab and added additional  color change

--- js/index.js
+++ js/index.js
@@ -1,7 +1,9 @@
 $(document).ready(function(){
     $("li").hover(function(){
-      $(this).css("background-color", "#f3f3f3");
-      }, function(){
       $(this).css("background-color", "#88f057");
+      $(this).css("color", "#000001");
+    }, function(){
+      $(this).css("background-color", "#000001");
+      $(this).css("color", "#f3f3f3");
     });
   });
